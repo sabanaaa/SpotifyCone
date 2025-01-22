ac94b5ed0c902307c9b26b0743bc5dbbe39c60c6
sabana <messyme154@gmail.com>
2025-01-22 14:36:49 +0530

    rjiho

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,23 @@ import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/home/Homepage";
 import AuthCallbackPage from "./pages/auth-callback/AuthCalbackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
-
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
 
 function App() {
   return (
     <>  
  <Routes>
-  <Route path='/' element={<Homepage />} /> 
- <Route	path='/sso-callback'element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}
+ <Route	
+ path='/sso-callback'
+ element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}
  />
 <Route path ='/auth-callback' element={<AuthCallbackPage/>} /> 
+
+<Route element={<MainLayout />}>
+<Route path='/' element={<Homepage/>}/>
+<Route path='/chat' element={<ChatPage />}/>
+</Route>
 </Routes>
     </>
   );
