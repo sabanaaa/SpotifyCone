b86de4eb28c1a488e90cd449a3666028108e6578
sabana <messyme154@gmail.com>
2025-01-24 21:53:57 +0530

    back

--- src/layout/components/LeftSidebar.tsx
+++ src/layout/components/LeftSidebar.tsx
@@ -1,9 +1,52 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { SignedIn } from "@clerk/clerk-react";
+import { HomeIcon, MessageCircle } from "lucide-react";
+import { Link } from "react-router-dom";
 
 
 const LeftSidebar = () => {
-  return (
-    <div>LeftSidebar</div>
-  )
-}
+  return <div className="h-full flex flex-colgap-2">
+<div className="rounded-lg bg-zinc-900 p-4">
+<div className="space-y-2">
+<Link 
+to ={"/"} 
+className={cn(
+  buttonVariants({
+  variant:"ghost",
+  className:"w-full justify-start text-white hover:bg-zinc-800",
+  })
+  )}
+>
+<HomeIcon className="mr-2 size-5"/>
+<span className="hidden md:inline">Home</span>
+</Link>
 
-export default LeftSidebar
+<SignedIn>
+<Link 
+to ={"/chat"} 
+className={cn(
+  buttonVariants( {
+  variant:"ghost",
+  className:"w-full justify-start text-white hover:bg-zinc-800",
+  })
+  )}
+>
+  <MessageCircle className="mr-2 size-5" />
+  <span className="hidden md:inline">Messages</span>
+
+</Link>
+  </SignedIn>
+</div>
+</div>
+
+<div className="flex-1 rounded-lg bg-zinc-900 p-4">
+  <div className="flex items-center">
+
+  </div>
+</div>
+  </div>
+  
+};
+
+export default LeftSidebar;
